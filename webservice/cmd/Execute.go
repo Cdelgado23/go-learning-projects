e4121117c4b09fb84740c46dce38ddc268e59487
Cdelgado23 <cdelgadoguiberteau@gmail.com>
2024-04-24 22:49:33 +0200

    add basic web server with hello endpoint

--- webservice/cmd/Execute.go
+++ webservice/cmd/Execute.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+const httpPort = ":8080"
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Starting web service on port", httpPort)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", helloHandler)
+
+	log.Fatal(http.ListenAndServe(httpPort, mux))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello, world!"))
 }
